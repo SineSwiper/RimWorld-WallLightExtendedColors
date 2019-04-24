6025b36167c3d4cb4adfc99a440c0b6b8c339040
Brendan Byrd <GitHub@ResonatorSoft.org>
2019-04-24 00:17:34 -0400

    Initial commit

---
+++ Source/WallLightExtendedColors/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WallLightExtendedColors")]
+[assembly: AssemblyDescription("Extended colors for the Murmur's Wall Light mod!")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WallLightExtendedColors")]
+[assembly: AssemblyCopyright("Copyright © SineSwiper, CC BY-SA 4.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("629bfd92-cb25-45b7-87d9-79e2e6e92465")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/WallLightExtendedColors/WallLightExtendedColors.cs
@@ -0,0 +1,81 @@
+﻿using Verse;
+
+namespace MURWallLight {
+
+    // Secondary colors
+
+    public class WallLightYellow : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerYellow";
+            this.moteStr = "Lighting_MURWallLightYellowVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightMagenta : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerMagenta";
+            this.moteStr = "Lighting_MURWallLightMagentaVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightCyan : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerCyan";
+            this.moteStr = "Lighting_MURWallLightCyanVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    // Tertiary colors
+
+    public class WallLightRose : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerRose";
+            this.moteStr = "Lighting_MURWallLightRoseVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightViolet : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerViolet";
+            this.moteStr = "Lighting_MURWallLightVioletVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightAzure : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerAzure";
+            this.moteStr = "Lighting_MURWallLightAzureVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightSpringGreen : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerSpringGreen";
+            this.moteStr = "Lighting_MURWallLightSpringGreenVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightChartreuse : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerChartreuse";
+            this.moteStr = "Lighting_MURWallLightChartreuseVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+    public class WallLightOrange : WallLight {
+        public override void ColorSetup() {
+            this.defStr = "Lighting_MURWallLightVII_GlowerOrange";
+            this.moteStr = "Lighting_MURWallLightOrangeVII_On";
+            this.onDef = ThingDef.Named(this.moteStr);
+        }
+    }
+
+}
